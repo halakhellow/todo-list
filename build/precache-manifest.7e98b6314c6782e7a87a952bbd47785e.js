569663e6c70e404c688b04b968d501d93ec52850
Hala Alkhellow <halakhellow1996@gmail.com>
2020-09-14 15:23:55 +0300

    Updates

---
+++ build/precache-manifest.7e98b6314c6782e7a87a952bbd47785e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e097b996a50ac8aa321571584fd771df",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "7f319cb2eb58073be0c0",
+    "url": "/todo-list/static/css/main.f0da34f5.chunk.css"
+  },
+  {
+    "revision": "9c0eb1505b0ec02c75ec",
+    "url": "/todo-list/static/js/2.005c8317.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.005c8317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f319cb2eb58073be0c0",
+    "url": "/todo-list/static/js/main.348200a7.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
