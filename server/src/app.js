dccb17a010db3022bb023d541fd5e650046a3490
Hala Alkhellow <halakhellow1996@gmail.com>
2022-09-06 21:42:11 +0300

    parse and encrypt cookies and verify the token for the api routes

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const morgan = require("morgan");
+const { expressjwt: jwt } = require("express-jwt");
+const cookieParser = require("cookie-parser");
+const { encryptCookieNodeMiddleware } = require("encrypt-cookie");
 const connectToMongo = require("./db/connection");
 const apiRoutes = require("./routes");
 
@@ -13,6 +16,19 @@ app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("tiny"));
+
+app.use(cookieParser(process.env.SECRET_KEY));
+app.use(encryptCookieNodeMiddleware(process.env.SECRET_KEY));
+
+app.use(
+  "/api",
+  jwt({
+    secret: process.env.SECRET_KEY,
+    algorithms: ["HS256"],
+    requestProperty: "user",
+    getToken: (req) => req.signedCookies("token") ?? req.cookies("token"),
+  }).unless({ path: ["/api/auth/google", "/api/auth/google/callback"] })
+);
 
 app.use("/api", apiRoutes);
 const port = process.env.NODE_LOCAL_PORT;
