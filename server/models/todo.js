774a1ebc29e18bba0aeacac6dc86702cf00ffb0a
Hala Alkhellow <halakhellow1996@gmail.com>
2022-09-06 03:25:29 +0300

    add todo model schema

---
+++ server/models/todo.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const todoSchema = new Schema(
+  {
+    task: { type: String, required: true },
+    completed: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    checked: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("todo", todoSchema);
