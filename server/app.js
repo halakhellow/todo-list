e124950a93055812e59f1eac5a0aff1d7a613b57
Hala Alkhellow <halakhellow1996@gmail.com>
2022-09-06 03:26:57 +0300

    define the base route in app.js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const connectToMongo = require("./db/connection");
+const apiRoutes = require("./routes");
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use("/api", apiRoutes);
 const port = process.env.NODE_LOCAL_PORT;
 
 app.listen(port, () => {
