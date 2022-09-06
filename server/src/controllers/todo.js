f4142f74d35c511cbedf75e08e719e0c59e6fec5
Hala Alkhellow <halakhellow1996@gmail.com>
2022-09-07 00:30:35 +0300

    define routes & controllers for editing and deleting todos

--- server/src/controllers/todo.js
+++ server/src/controllers/todo.js
@@ -4,10 +4,11 @@ const todoControllers = {};
 
 todoControllers.getTodos = async (req, res) => {
   try {
-    const todos = await Todos.find();
-    if (todos.length === 0)
+    const userId = req.user._id;
+    const userTodos = await Todos.find({ user: userId });
+    if (userTodos.length === 0)
       res.status(404).json({ message: "No todos to show" });
-    else res.json(todos);
+    else res.json(userTodos);
   } catch (err) {
     res.json({ error: err.message });
   }
@@ -24,6 +25,47 @@ todoControllers.addTodo = async (req, res) => {
       user: userId,
     });
     res.status(201).json(newTodo);
+  } catch (err) {
+    res.json({ error: err.message });
+  }
+};
+
+todoControllers.editTodo = async (req, res) => {
+  try {
+    const { todoid } = req.params;
+    const { task, completed } = req.body;
+    const userId = req.user._id;
+    if (!task) res.status(400).json({ message: "field can't be empty" });
+    else {
+      const todo = await Todos.findOne({ _id: todoid, user: userId });
+      if (todo) {
+        const updatedTodo = await Todos.findByIdAndUpdate(
+          todoid,
+          {
+            task,
+            completed,
+          },
+          {
+            new: true,
+          }
+        );
+        res.json(updatedTodo);
+      } else res.status(403).json({ message: "Forbidden" });
+    }
+  } catch (err) {
+    res.json({ error: err.message });
+  }
+};
+
+todoControllers.deleteTodo = async (req, res) => {
+  try {
+    const { todoid } = req.params;
+    const userId = req.user._id;
+    const todo = await Todos.findOne({ _id: todoid, user: userId });
+    if (todo) {
+      await Todos.findByIdAndDelete(todoid);
+      res.json(await Todos.find());
+    } else res.status(403).json({ message: "Forbidden" });
   } catch (err) {
     res.json({ error: err.message });
   }
--- server/src/routes/todo.js
+++ server/src/routes/todo.js
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.get("/", todoControllers.getTodos);
 router.post("/", todoControllers.addTodo);
+router.put("/:todoid", todoControllers.editTodo);
+router.delete("/:todoid", todoControllers.deleteTodo);
 
 module.exports = router;
