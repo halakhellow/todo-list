ed14757d5583c808cbad458d578195641ec5a262
Hala Alkhellow <halakhellow1996@gmail.com>
2022-09-10 02:37:33 +0300

    use react-cookie to check for authentication and render the proper components

--- client/src/components/TodoItem/TodoItem.js
+++ client/src/components/TodoItem/TodoItem.js
@@ -3,7 +3,7 @@ import { useQueryClient, useMutation } from "react-query";
 import { editTodoRequest, deleteTodoRequest } from "../../functions";
 import "./TodoItem.css";
 
-const TodoItem = ({ todo, provided, innerRef }) => {
+const TodoItem = ({ todo }) => {
   const queryClient = useQueryClient();
   const [edition, setEdition] = useState(false);
   const [newTask, setNewTask] = useState(todo.task);
@@ -37,11 +37,7 @@ const TodoItem = ({ todo, provided, innerRef }) => {
     setEdition(false);
   };
   return (
-    <div
-      {...provided.draggableProps}
-      {...provided.dragHandleProps}
-      ref={innerRef}
-    >
+    <div>
       {edition ? (
         <form className="TodoItem-edit" onSubmit={handleSubmit}>
           <input value={newTask} onChange={handleChange} />
@@ -51,24 +47,19 @@ const TodoItem = ({ todo, provided, innerRef }) => {
         </form>
       ) : (
         <div className="TodoItem">
-          <div>
-            <span className="fas fa-grip-horizontal"></span>
-            <label
-              htmlFor="check"
-              className={`container ${todo.completed ? "checked" : ""}`}
-            >
-              <span className="todo-task">{todo.task}</span>
-              <input
-                id="check"
-                type="checkbox"
-                onClick={() =>
-                  editTodo({ ...todo, completed: !todo.completed })
-                }
-                checked={todo.completed}
-              />
-              <span className="checkmark"></span>
-            </label>
-          </div>
+          <label
+            htmlFor="check"
+            className={`container ${todo.completed ? "checked" : ""}`}
+          >
+            <span className="todo-task">{todo.task}</span>
+            <input
+              id="check"
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => editTodo({ ...todo, completed: !todo.completed })}
+            />
+            <span className="checkmark"></span>
+          </label>
           <div className="icons">
             <button onClick={handleEdit}>
               <i className="fa fa-pencil"></i>
--- client/src/components/TodoList/TodoList.js
+++ client/src/components/TodoList/TodoList.js
@@ -1,52 +1,35 @@
 import React from "react";
 import { useQuery } from "react-query";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { useCookies } from "react-cookie";
 
 import Loader from "../Loader/Loader";
 import TodoItem from "../TodoItem/TodoItem";
 import TodoForm from "../TodoForm/TodoForm";
+import GoogleSignInBen from "../GoogleSignInBtn/GoogleSignInBtn";
 
 import { getTodosRequest } from "../../functions";
 
 import thumbtack from "../../images/thumbtack.png";
 
 import "./TodoList.css";
+import LogOutBtn from "../LogOutBtn/LogOutBtn";
 
 const TodoList = () => {
-  const { isLoading, data: todos } = useQuery("todos", getTodosRequest);
+  const [cookies, setCookie] = useCookies(["token"]);
+  let { isLoading, data: todos } = useQuery("todos", getTodosRequest);
 
-  const handleOnDragEnd = (result) => {
-    if (!result.destination) return;
-    if (
-      result.source.droppableId === result.destination.droppableId &&
-      result.source.index === result.destination.index
-    )
-      return;
-    const updatedTodos = Array.from(todos);
-    const [reorderedItem] = updatedTodos.splice(result.source.index, 1);
-    updatedTodos.splice(result.destination.index, 0, reorderedItem);
-
-    // setTodos(updatedTodos);
-  };
   let todolist;
+
   if (!isLoading) {
     !todos
       ? (todolist = (
           <h2 className="no-todos">
             Let's get things <span>DONE</span> today
-            <i class="fa fa-lightbulb-o" aria-hidden="true"></i>
+            <span class="circle yellow"></span>
           </h2>
         ))
-      : (todolist = todos.map((todo, index) => (
-          <Draggable key={todo._id} draggableId={todo.task} index={index}>
-            {(provided) => (
-              <TodoItem
-                innerRef={provided.innerRef}
-                provided={provided}
-                todo={todo}
-              />
-            )}
-          </Draggable>
+      : (todolist = todos.map((todo) => (
+          <TodoItem key={todo._id} todo={todo} />
         )));
   }
   return (
@@ -57,16 +40,15 @@ const TodoList = () => {
       </h1>
       <div className="TodoList">
         <TodoForm />
-        <DragDropContext onDragEnd={handleOnDragEnd}>
-          <Droppable droppableId="todo-items">
-            {(provided) => (
-              <ul {...provided.droppableProps} ref={provided.innerRef}>
-                {isLoading ? <Loader /> : todolist}
-                {provided.placeholder}
-              </ul>
-            )}
-          </Droppable>
-        </DragDropContext>
+        {isLoading ? (
+          <Loader />
+        ) : cookies.token ? (
+          <>
+            {todolist} <LogOutBtn />
+          </>
+        ) : (
+          <GoogleSignInBen />
+        )}
       </div>
     </div>
   );
